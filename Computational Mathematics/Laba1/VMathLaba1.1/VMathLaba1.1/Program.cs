edfc4fdf67118550d3906b7fff74952df1f34aac
44917218+Dioneya@users.noreply.github.com <44917218+Dioneya@users.noreply.github.com>
2021-09-19 14:12:51 +0300

    CompMath Laba1

---
+++ Computational Mathematics/Laba1/VMathLaba1.1/VMathLaba1.1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace VMathLaba1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Машинный эпсилон для типа float, полученный при расчетах: ");
+            PrintColoredText(string.Format("{0}", GetFloatMachineEpsilon()), ConsoleColor.Cyan);
+
+            Console.Write("\nМашинный эпсилон для типа float в C#: ");
+            PrintColoredText(string.Format("{0}\n", float.Epsilon), ConsoleColor.Cyan);
+
+            Console.Write("\nМашинный эпсилон для типа double, полученный при расчетах: ");
+            PrintColoredText(string.Format("{0}", GetDoubleMachineEpsilon()), ConsoleColor.Cyan);
+
+            Console.Write("\nМашинный эпсилон для типа double в C#: ");
+            PrintColoredText(string.Format("{0}\n", double.Epsilon), ConsoleColor.Cyan);
+
+            Console.Write("\nМашинная бесконечность для типа float равна: ");
+            PrintColoredText(string.Format("{0}\n", GetFloatMachineInfinity()), ConsoleColor.Cyan);
+
+            Console.Write("\nМашинная бесконечность для типа double: ");
+            PrintColoredText(string.Format("{0}", GetDoubleMachineInfinity()), ConsoleColor.Cyan);
+            Console.ReadKey();
+        }
+
+        private static void PrintColoredText(string text, ConsoleColor color) 
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+
+        private static double GetDoubleMachineEpsilon() 
+        {
+            double epsilon = 1.0;
+            double value = 1.0;
+            while (value + 1 > 1)
+            {
+                value /= 2;
+                if (value != 0)
+                {
+                    epsilon = value;
+                }
+            }
+
+            return epsilon;
+        }
+
+        private static float GetFloatMachineEpsilon() 
+        {
+            float epsilon = 1.0f;
+            float value = 1.0f;
+            while (value + 1 > 1)
+            {
+                value /= 2;
+                if (value != 0)
+                {
+                    epsilon = value;
+                }
+            }
+
+            return epsilon;
+        }
+
+        private static float GetFloatMachineInfinity()
+        {
+            float infinity = 0.1f;
+            float value = 0.1f;
+            while (true)
+            {
+                value *= 2;
+
+                if (value == float.PositiveInfinity) break;
+
+                infinity = value;
+            }
+
+            return infinity;
+        }
+
+        private static double GetDoubleMachineInfinity() 
+        {
+            double infinity = 0.1;
+            double value = 0.1;
+            while (true)
+            {
+                value *= 2;
+
+                if (value == double.PositiveInfinity) break;
+
+                infinity = value;
+            }
+
+            return infinity;
+        }
+    }
+}
---
+++ Computational Mathematics/Laba1/VMathLaba1.2/VMathLaba1.2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace VMathLaba1._2
+{
+    class Program
+    {
+        public enum FactorialMode
+        {
+            None,
+            Integer,
+            Real
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Без изменения формулы {0}", ErrorFucn(3, FactorialMode.None));
+            Console.WriteLine("Расчёт факториала с числом целого типа {0}", ErrorFucn(3, FactorialMode.Integer));
+            Console.WriteLine("Расчёт факториала с числом вещественного типа {0}", ErrorFucn(3, FactorialMode.Real));
+            Console.WriteLine("Расчёт формулы через предыдущий член ряда {0}", ErrorFuncModify(3));
+            Console.ReadKey();
+        }
+
+        private static int Factorial(int n)
+        {
+            int factorial = 1;
+
+            if (n == 0)
+                return 1;
+
+            for (int i = 1; i <= n; i++)
+                factorial *= i;
+
+            return factorial;
+        }
+
+        private static double Factorial(double n)
+        {
+            double factorial = 1;
+            try
+            {
+                if (n == 0)
+                    return 1;
+                else
+                    for (int i = 0; i <= n; i++)
+                        factorial *= i;
+            }
+            catch (StackOverflowException e)
+            {
+                Console.WriteLine(e.Message);
+                return 1;
+            }
+            return factorial;
+        }
+
+        public static double ErrorFucn(double x, FactorialMode factorialMode)
+        {
+            double sum = 0;
+
+            for (int n = 0; ; n++)
+            {
+                double res = Math.Pow(-1, n) * Math.Pow(x, 2 * n + 1) / (factorialMode == FactorialMode.Real ? Factorial((double)n) : Factorial(n) * (2 * n + 1));
+                if (double.IsInfinity(res)) break;
+                sum += res;
+            }
+
+            return 2 / Math.Sqrt(Math.PI) * sum;
+        }
+
+        public static double ErrorFuncModify(double x) 
+        {
+            double sum = 0;
+            double prevElem = (-1)*x; // Потому что a(0) = -x т.е. каждый первый элемент будет равным -x
+
+            for (int n = 1; ; n++) 
+            {
+                double k = GetCoefficient(x,n);
+                double res = k * prevElem;
+                if (res == 0)
+                    break;
+                prevElem = res;
+                sum += res;
+            }
+
+            return 2 / Math.Sqrt(Math.PI) * sum;
+        }
+
+        private static double GetCoefficient(double x, int n)
+        {
+            return (-1)*Math.Pow(x, 2) * (2 * n - 1) / (n*(2*n+1));
+        } 
+
+    }
+}
